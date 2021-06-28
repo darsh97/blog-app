34cd14216aedbc4c7b813e7bc37b8e980fa45c41
Darshan <aravinddarshan13897@outlook.com>
2021-06-28 15:09:05 +0530

    Added main.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,4 @@
-# -*- coding: utf-8 -*-
-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "<h1> Hello world </h1>"
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
