c91d191373f8376308f3ef6c7e30f05ccb66b254
Darshan <aravinddarshan13897@outlook.com>
2021-06-28 15:53:52 +0530

    save

--- blueprints/blog.py
+++ blueprints/blog.py
@@ -1,8 +1,3 @@
 from flask import Blueprint
 
-blog = Blueprint('blog', __name__)
-
-@blog.route('/')
-def home():
-    return "<h1> Blog home page"
-    
+blog = Blueprint('blog', __name__, template_folder='templates')
